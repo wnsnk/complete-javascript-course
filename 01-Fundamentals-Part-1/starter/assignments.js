30a94b6a8adf7f327fc191342f8850a21d85a468
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-06-22 18:45:24 +0200

    lecture 10

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,10 @@
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = 'Netherlands';
+let continent = 'Europe';
+let population = 18000000;
+console.log(country);
+console.log(continent);
+console.log(population);
