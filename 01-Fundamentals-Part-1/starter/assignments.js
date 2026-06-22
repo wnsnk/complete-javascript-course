c089be204f637c9bc5958b2ebf90fc8da40132b1
wnsnk <236621219+wnsnk@users.noreply.github.com>
2026-06-22 23:27:59 +0200

    assignments lecture 11 and 12

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,10 +1,31 @@
+// Lecture 9 Values and Variables
 // Declare variables called country, continent and population and assign their values according to your own country (population in millions).
 
 // Log their values to the console.
 
-let country = 'Netherlands';
-let continent = 'Europe';
+const country = 'Netherlands';
+const continent = 'Europe';
 let population = 18000000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// Lecture 11 Data Types
+
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// Log the types of isIsland, population, country and language to the console.
+const isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
+
+// Lecture 12 let, const and var
+
+// Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+// Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+// Try to change one of the changed variables now, and observe what happens.
+
+language = 'Dutch'
